64e09478f0883ea0b046486bcae49d9339542672
Zephyr <ryanwang96@hotmail.com>
2018-09-15 11:59:12 +0800

    reduceInteger-complete

--- chapter3/readme.md
+++ chapter3/readme.md
@@ -28,8 +28,17 @@ if (index < blockDim.x);
 ### 2.展开循环:  
 将循环由for i in range(100): a[i] + b[i] 展开为 for i in np.linspace(0,99,50): a[i]+b[i];a[i+1] + b[i+1]  
 这种提升来源于编译器循环展开时减少指令消耗,增加更多的独立调度命令. 增加流水线上的并发操作, 增大内存带宽.  
-1)手动展开: kernel4为reduceUnrolling2,每个线程里手动展开两个数据块,这样理论上是由一个线程块处理两个数据块,
-在一个线程中有更多的独立内存加载/存储操作会产生更好的性能,这样可以更好地隐藏内存延迟,提高内存吞吐量. reduceUnrolling2的处理速度为0.0068sec,相比reduceInterleaved再次提高一倍性能.  
+1) 手动展开: kernel4为reduceUnrolling2,每个线程里手动展开两个数据块,这样理论上是由一个线程块处理两个数据块, 在一个线程中有更多的独立内存加载/存储操作会产生更好的性能,这样可以更好地隐藏内存延迟,提高内存吞吐量.  
+reduceUnrolling2的处理速度为0.0068sec,相比reduceInterleaved再次提高一倍性能. 而reduceUnrolling4的处理速度为0.0035sec, 再次提高几乎一倍. 可以使用nvprof --metrics dram_read_throughput ./reduceInteger 查看设备内存读取吞吐量.  
+2) 线程束的展开: 减少内存阻塞. 原有的__syncthreads被用于块内同步,它用来确保在线程在进入下一轮前,每一轮的所有线程已经将局部结果写入全局内存里了. 而当剩下的线程小于32个时, 每条指令会存在隐式的线程束内同步过程. 因此归约的最后6个迭代可以展开如 vmem[tid] += vmem[tid+32]; ... 等所示.  
+线程束展开能避免执行循环控制和线程同步逻辑. 笔者验证速度为0.0026sec,速度再次提升.  
+内存阻塞程度可由nvprof --metrics stall_sync ./reduceInteger来查看, 经验证UnrollWarps4为34.58% 而 Unrolling4为52.32%.  
+3) 循环完全展开: 已知一个循环中的迭代次数, 就可以把循环完全展开. 书中叙述该循环展开kernel较上述UnrollWarps4更快, 但经笔者本地实验, 完全展开反而略微降低了速度. 查看阻塞程度, CompleteUnrollWarps4为35.61%, 略高于UnrollWarps4的34.46%.  
+4) 模板函数: 使用模板参数替代块大小作为if语句的判断来展开循环, 好处在于编译时如果if语句条件为False会被直接删除,使内循环更有效率.  
+笔者实验证明其运行速度为0.0025sec,略有提升. 可以使用 nvprof --metrics gld_efficiecny,gst_efficiency ./reduceInteger查看内存的加载和存储效率.  
+
+
+
 
 
 
--- chapter3/reduceInteger.cu
+++ chapter3/reduceInteger.cu
@@ -101,11 +101,12 @@ __global__ void reduceInterleaved (int *g_idata, int *g_odata, unsigned int n){
 // kernel 4
 __global__ void reduceUnrolling2(int *g_idata,int *g_odata, unsigned int n){
     // set thread ID, one threadblock operates on two datablocks
+    // so that blockidx * 2 * blockdim.x
     unsigned int tid = threadIdx.x;
     unsigned int idx = blockIdx.x * blockDim.x * 2 + threadIdx.x;    
     // convert global data pointer to the local pointer of this block
     int *idata = g_idata + blockIdx.x * blockDim.x * 2;
-    // unrolling 2 data blocks
+    // unrolling 2 data blocks, add data from an adjacent data block
     if (idx + blockDim.x < n) g_idata[idx] += g_idata[idx+blockDim.x];
     __syncthreads();
     // inplace reduction in global memory
@@ -118,6 +119,148 @@ __global__ void reduceUnrolling2(int *g_idata,int *g_odata, unsigned int n){
         }
     // write result for this block to global memory
     if (tid ==0) g_odata[blockIdx.x] = idata[0];
+    }
+
+// kernel 5
+__global__ void reduceUnrolling4(int *g_idata,int *g_odata,unsigned int n){
+    // set threadId, one threadBlock operates on four dataBlocks
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x * 4 + threadIdx.x;
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x * 4;
+    // unrolling 4 data blocks
+    if (idx + 3 * blockDim.x < n){
+        int a1 = g_idata[idx];
+        int a2 = g_idata[idx + blockDim.x];
+        int a3 = g_idata[idx + blockDim.x * 2];
+        int a4 = g_idata[idx + blockDim.x * 3];
+        g_idata[idx] = a1 + a2 + a3 + a4;
+        }
+    __syncthreads();
+    // in-place reduction in global memory
+    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1){
+        if (tid < stride){
+            idata[tid] += idata[tid+stride];
+            }
+        __syncthreads();
+        }
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+    }
+
+// kernel 6
+__global__ void reduceUnrollWarps4(int *g_idata,int *g_odata,unsigned int n){
+    // set threadId, one threadBlock operates on four dataBlocks
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x * 4 + threadIdx.x;
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x * 4;
+    // unrolling 4 data blocks
+    if (idx + 3 * blockDim.x < n){
+        int a1 = g_idata[idx];
+        int a2 = g_idata[idx + blockDim.x];
+        int a3 = g_idata[idx + blockDim.x * 2];
+        int a4 = g_idata[idx + blockDim.x * 3];
+        g_idata[idx] = a1 + a2 + a3 + a4;
+        }
+    __syncthreads();
+    // in-place reduction in global memory
+    for (int stride = blockDim.x / 2; stride > 32; stride >>= 1){
+        if (tid < stride){
+            idata[tid] += idata[tid+stride];
+            }
+        __syncthreads();
+        }
+
+    // unrolling warp
+    if (tid <32){
+        volatile int *vmem = idata;
+        vmem[tid] += vmem[tid+32];
+        vmem[tid] += vmem[tid+16];
+        vmem[tid] += vmem[tid+8];
+        vmem[tid] += vmem[tid+4];
+        vmem[tid] += vmem[tid+2];
+        vmem[tid] += vmem[tid+1];
+        }
+
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+    }
+
+// kernel 7
+__global__ void reduceCompleteUnrollWarps4(int *g_idata, int *g_odata, unsigned int n){
+    // set threadId
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x * 4 + threadIdx.x;
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x * 4;
+    // unrolling 4
+    if (idx + 3*blockDim.x < n){
+        int a1 = g_idata[idx];
+        int a2 = g_idata[idx+blockDim.x];
+        int a3 = g_idata[idx+2*blockDim.x];
+        int a4 = g_idata[idx+3*blockDim.x];
+        g_idata[idx] = a1+a2+a3+a4;
+        }
+    __syncthreads();
+    // in-place reduction and complete unroll
+    if (blockDim.x>=1024 && tid < 512) idata[tid] += idata[tid+512];
+    __syncthreads();
+    if (blockDim.x>=512 && tid < 256)  idata[tid] += idata[tid+256];
+    __syncthreads();
+    if (blockDim.x>=256 && tid < 128)  idata[tid] += idata[tid+128];
+    __syncthreads();
+    if (blockDim.x>=128 && tid < 64)   idata[tid] += idata[tid+64];
+    __syncthreads();
+    // unrolling warp
+    if (tid < 32) {
+        volatile int *vsmem = idata;
+        vsmem[tid] += vsmem[tid+32];
+        vsmem[tid] += vsmem[tid+16];
+        vsmem[tid] += vsmem[tid+ 8];
+        vsmem[tid] += vsmem[tid+ 4];
+        vsmem[tid] += vsmem[tid+ 2];
+        vsmem[tid] += vsmem[tid+ 1];
+        }
+    // write result
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+    }
+
+
+template <unsigned int iBlockSize>
+__global__ void reduceCompleteUnroll(int *g_idata, int *g_odata, unsigned int n){
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x * 4 + threadIdx.x;
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x * 4;
+    // unrolling 4
+    if (idx + 3 * blockDim.x < n){
+        int a1 = g_idata[idx];
+        int a2 = g_idata[idx + blockDim.x];
+        int a3 = g_idata[idx + blockDim.x * 2];
+        int a4 = g_idata[idx + blockDim.x * 3];
+        g_idata[idx] = a1 + a2 + a3 + a4;
+        }
+    __syncthreads(); 
+    // in-place reduction and complete unroll
+    if (iBlockSize>=1024 && tid < 512) idata[tid] += idata[tid+512];
+    __syncthreads();
+    if (iBlockSize>=512 && tid < 256)  idata[tid] += idata[tid+256];
+    __syncthreads();
+    if (iBlockSize>=256 && tid < 128)  idata[tid] += idata[tid+128];
+    __syncthreads();
+    if (iBlockSize>=128 && tid < 64)   idata[tid] += idata[tid+64];
+    __syncthreads();
+    // unrolling warp
+    if (tid < 32) {
+        volatile int *vsmem = idata;
+        vsmem[tid] += vsmem[tid+32];
+        vsmem[tid] += vsmem[tid+16];
+        vsmem[tid] += vsmem[tid+ 8];
+        vsmem[tid] += vsmem[tid+ 4];
+        vsmem[tid] += vsmem[tid+ 2];
+        vsmem[tid] += vsmem[tid+ 1];
+        }
+    // write result
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
     }
 
 int main(int argc, char** argv){
@@ -221,6 +364,76 @@ int main(int argc, char** argv){
     for(int i=0;i<grid.x/2;i++) gpu_sum += h_odata[i];
     printf("gpu Unrolling2 elaped %f sec gpu_sum %d <<<grid %d block %d>>>\n",
         iElaps,gpu_sum,grid.x/2,block.x);
+    
+    // kernel 5: reduceUnrolling4
+    cudaMemcpy(d_idata,h_idata,bytes,cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = seconds();
+    reduceUnrolling4<<<grid.x/4,block>>>(d_idata,d_odata,size);
+    cudaDeviceSynchronize();
+    iElaps = seconds() - iStart;
+    cudaMemcpy(h_odata,d_odata,(grid.x/4)*sizeof(int),cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for(int i=0;i<grid.x/4;i++) gpu_sum += h_odata[i];
+    printf("gpu Unrolling4 elaped %f sec gpu_sum %d <<<grid %d block %d>>>\n",
+        iElaps,gpu_sum,grid.x/4,block.x);
+    
+    // kernel 6: reduceUnrollWarps4
+    cudaMemcpy(d_idata,h_idata,bytes,cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = seconds();
+    reduceUnrollWarps4<<<grid.x/4,block>>>(d_idata,d_odata,size);
+    cudaDeviceSynchronize();
+    iElaps = seconds() - iStart;
+    cudaMemcpy(h_odata,d_odata,(grid.x/4)*sizeof(int),cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for(int i=0;i<grid.x/4;i++) gpu_sum += h_odata[i];
+    printf("gpu UnrollWarps4 elaped %f sec gpu_sum %d <<<grid %d block %d>>>\n",
+        iElaps,gpu_sum,grid.x/4,block.x);
+    
+    // kernel 7: reduceCompleteUnrollWarps4
+    cudaMemcpy(d_idata,h_idata,bytes,cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = seconds();
+    reduceCompleteUnrollWarps4<<<grid.x/4,block>>>(d_idata,d_odata,size);
+    cudaDeviceSynchronize();
+    iElaps = seconds() - iStart;
+    cudaMemcpy(h_odata,d_odata,(grid.x/4)*sizeof(int),cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for(int i=0;i<grid.x/4;i++) gpu_sum += h_odata[i];
+    printf("gpu CompleteUnrollWarps4 elaped %f sec gpu_sum %d <<<grid %d block %d>>>\n",
+        iElaps,gpu_sum,grid.x/4,block.x);
+    
+    // kernel 8: reduceCompleteUnroll (template function)
+    cudaMemcpy(d_idata,h_idata,bytes,cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = seconds();
+    switch (blocksize)
+    {
+    case 1024:
+        reduceCompleteUnroll<1024><<<grid.x/4,block>>>(d_idata,d_odata,size);
+        break;
+    case 512:
+        reduceCompleteUnroll<512><<<grid.x/4,block>>>(d_idata,d_odata,size);
+        break;
+    case 256:
+        reduceCompleteUnroll<256><<<grid.x/4,block>>>(d_idata,d_odata,size);
+        break;
+    case 128:
+        reduceCompleteUnroll<128><<<grid.x/4,block>>>(d_idata,d_odata,size);
+        break;
+    case 64:
+        reduceCompleteUnroll<64><<<grid.x/4,block>>>(d_idata,d_odata,size);
+        break;
+    }
+    cudaDeviceSynchronize();
+    iElaps = seconds() - iStart;
+    cudaMemcpy(h_odata,d_odata,(grid.x/4)*sizeof(int),cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for(int i=0;i<grid.x/4;i++) gpu_sum += h_odata[i];
+    printf("gpu CompleteUnroll<template> elaped %f sec gpu_sum %d <<<grid %d block %d>>>\n",
+        iElaps,gpu_sum,grid.x/4,block.x);
+
 
     free(h_idata);
     free(h_odata);
